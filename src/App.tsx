d62dcb1b25b1a72284c07ccfc0f276226b51fc33
singuerinc <nahuel.scotti@gmail.com>
2022-10-07 16:08:06 +0200

    feat: format

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,13 @@ function App() {
 
   return (
     <>
-      <div className="flex h-full w-full select-none items-center justify-center">
-        <div className="flex w-full items-center justify-center whitespace-nowrap text-[27vw] tabular-nums leading-none">
+      <div className="flex h-full w-full select-none flex-col items-center justify-center font-medium tabular-nums">
+        <div className="text-">{format(time, "d/M")}</div>
+        <div className="flex w-full items-center justify-center whitespace-nowrap text-[27vw] leading-none">
           <span className="flex">{format(time, "H")}</span>
-          <span className="text-light flex text-[0.1em]">{format(time, "ss")}</span>
+          <span className="text-light flex w-[3vw] justify-end text-[0.1em] text-red-600">
+            {format(time, "ss")}
+          </span>
           <span className="flex">{format(time, "mm")}</span>
         </div>
       </div>
