12806742a21a09b59b95a37ed23889d347d64f3a
Nahuel Scotti <nahuel.scotti@gmail.com>
2024-07-03 14:05:59 +0200

    feat: with blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
+import cn from "classnames";
 import { format } from "date-fns";
-import { CloudLightning } from "lucide-react";
-import React from "react";
+import React, { type PropsWithChildren } from "react";
 import { Outlet } from "react-router-dom";
 import { useTheme } from "./useTheme";
 import { useTimer } from "./useTimer";
 
-function Block({ title, data }: { title?: string; data: React.ReactNode }) {
+function Block({
+  title,
+  data,
+  children,
+}: PropsWithChildren<{ title?: string; data: React.ReactNode }>) {
   return (
-    <div className="flex aspect-video w-full flex-col gap-y-2 rounded-xl bg-zinc-800 p-6">
-      {title && <h3 className="text-xl font-normal before:empty:inline-block">{title}</h3>}
-      <h4 className="text-2xl font-semibold">{data}</h4>
+    <div className="flex aspect-video w-fit flex-col gap-y-4 self-start rounded-xl bg-neutral-200/30 p-6 dark:bg-neutral-800/30">
+      <div className="flex flex-col gap-y-1">
+        {title && (
+          <h3 className="text-xs font-semibold text-neutral-500 before:empty:inline-block">
+            {title}
+          </h3>
+        )}
+        <h4 className="text-2xl font-medium text-neutral-900 dark:text-neutral-300">{data}</h4>
+      </div>
+      {children}
     </div>
   );
 }
@@ -20,26 +31,77 @@ function App() {
   const timer = useTimer();
   const { time } = timer;
 
+  const timeStr = format(time, "H:mm:ss");
+  const dayStr = format(time, "EEEE");
+  const dayNumStr = format(time, "d");
+  const dayNum = Number.parseInt(format(time, "d"), 10);
+  const monthNum = Number.parseInt(format(time, "M"), 10);
+  const weekNum = Number.parseInt(format(time, "w"), 10);
+  const quarterStr = format(time, "qqq");
+  const quarterNum = Number.parseInt(format(time, "q"), 10);
+  // const amOrPm = format(time, "a");
+
   return (
     <>
       <div className="flex h-full w-full select-none flex-col items-center justify-center gap-8 p-12 font-medium tabular-nums md:p-0">
-        <div
-          className="flex w-full items-center justify-center whitespace-nowrap leading-none"
-          style={{ fontSize: "12vw" }}
-        >
-          <span className="flex">{format(time, "H mm ss")}</span>
+        <div className="flex w-full items-center justify-center whitespace-nowrap leading-none">
+          <div className="flex">
+            <div className="text-neutral-900 dark:text-orange-300" style={{ fontSize: "12vw" }}>
+              {timeStr}
+            </div>
+            {/* <div className="flex flex-col gap-y-2 self-center text-orange-900 dark:text-orange-500">
+              <div>{amOrPm}</div>
+            </div> */}
+          </div>
         </div>
-        <div className="grid grid-cols-2 gap-2 md:grid-cols-3">
-          <Block data={format(time, "EEEE")} />
-          <Block data={format(time, "dd")} />
-          <Block data={format(time, "MMMM")} />
-          <Block title="Week" data={format(time, "w")} />
+        <div className="flex flex-wrap gap-2">
+          <Block title="Day name" data={dayStr}>
+            <div className="flex w-fit gap-x-3">
+              <Circles total={7} current={dayNum} />
+            </div>
+          </Block>
+          <Block title="Day" data={dayNumStr}>
+            <div className="grid w-fit grid-cols-7 gap-3">
+              <Circles total={31} current={dayNum} />
+            </div>
+          </Block>
+          <Block title="Month" data={format(time, "MMMM")}>
+            <div className="grid w-fit grid-cols-3 gap-3">
+              <Circles total={12} current={monthNum} />
+            </div>
+          </Block>
           <Block title="Year" data={format(time, "yyyy")} />
-          <Block title="Quarter" data={format(time, "qqq")} />
-          <Block title="Weather" data={<CloudLightning size={48} />} />
+          <Block title="Week" data={format(time, "w")}>
+            <div className="grid w-fit grid-cols-7 gap-3">
+              <Circles total={52} current={weekNum} />
+            </div>
+          </Block>
+          <Block title="Quarter" data={quarterStr}>
+            <div className="grid w-fit grid-cols-4 gap-3">
+              <Circles total={4} current={quarterNum} />
+            </div>
+          </Block>
         </div>
       </div>
       <Outlet />
+    </>
+  );
+}
+
+function Circles({ total, current }: { total: number; current: number }) {
+  return (
+    <>
+      {Array.from({ length: total }).map((_, i) => (
+        <div
+          key={i}
+          className={cn({
+            "h-3 w-3 rounded-full": true,
+            "bg-orange-600/10 dark:bg-orange-900/70": current > i + 1,
+            "bg-neutral-200 dark:bg-neutral-900": current < i + 1,
+            "bg-orange-500 dark:bg-orange-500": current === i + 1,
+          })}
+        />
+      ))}
     </>
   );
 }
