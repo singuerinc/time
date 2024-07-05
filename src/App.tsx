452a5bb2a6033a4cc26c7e62e3133002d1b3b4d7
Nahuel Scotti <nahuel.scotti@gmail.com>
2024-07-05 15:43:59 +0200

    feat: update themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { useLocalStorage } from "@uidotdev/usehooks";
 import cn from "classnames";
 import { format, getDaysInMonth, getISOWeeksInYear } from "date-fns";
-import React, { useState, type PropsWithChildren } from "react";
+import { SwatchBookIcon } from "lucide-react";
+import React, { useCallback, useMemo, type PropsWithChildren } from "react";
 import { Outlet } from "react-router-dom";
 import { useTheme } from "./useTheme";
 import { useTimer } from "./useTimer";
@@ -8,17 +10,37 @@ import { useTimer } from "./useTimer";
 function Block({
   title,
   data,
+  theme,
   children,
-}: PropsWithChildren<{ title?: string; data: React.ReactNode }>) {
+}: PropsWithChildren<{ title?: string; data: React.ReactNode;theme: Theme }>) {
+  const themes = {
+    orange: {
+      title:{"text-orange-400": true},
+      data: {"text-neutral-900 dark:text-orange-200": true}
+    },
+    pink: {
+      title:{"text-pink-400": true},
+      data: {"text-neutral-900 dark:text-pink-200": true}
+    },
+    teal: {
+      title:{"text-teal-600": true},
+      data:{"text-neutral-900 dark:text-teal-200": true}
+    },
+    cyan: {
+      title:{"text-cyan-400": true},
+      data: {"text-neutral-900 dark:text-cyan-200": true}
+    },
+  } satisfies Record<Theme, {title:Record<string, boolean>, data:Record<string, boolean>}>;
+  
   return (
     <div className="flex aspect-video w-fit flex-col gap-y-4 self-start rounded-xl bg-neutral-200/30 p-6 dark:bg-neutral-800/30">
       <div className="flex flex-col gap-y-1">
         {title && (
-          <h3 className="text-xs font-semibold text-zinc-500 before:empty:inline-block">
+          <h3 className={cn("text-xs font-semibold  before:empty:inline-block", themes[theme].title)}>
             {title}
           </h3>
         )}
-        <h4 className="text-2xl font-medium text-orange-900 dark:text-orange-300">{data}</h4>
+        <h4 className={cn("text-2xl font-medium", themes[theme].data)}>{data}</h4>
       </div>
       {children}
     </div>
@@ -32,10 +54,17 @@ enum Theme {
   "cyan" = "cyan",
 }
 
+const themes = {
+  0: Theme.orange,
+  1: Theme.teal,
+  2: Theme.pink,
+  3: Theme.cyan,
+} as Record<number, Theme> satisfies Record<number, Theme>;
+
 function App() {
   useTheme();
-
-  const [theme] = useState<Theme>(Theme.orange);
+  const [themeIndex, setThemeIndex] = useLocalStorage<number>("themeIndex", 0);
+  const theme = useMemo<Theme>(() => themes[themeIndex], [themeIndex])
 
   const timer = useTimer();
   const { time } = timer;
@@ -50,44 +79,47 @@ function App() {
   const quarterNum = Number.parseInt(format(time, "q"), 10);
   const weeksInYear = getISOWeeksInYear(time);
   const daysInMonth = getDaysInMonth(time);
-  // const amOrPm = format(time, "a");
+
+  const toggleTheme = useCallback(() => {
+    setThemeIndex((themeIndex + 1) % 4);
+  }, [theme])
 
   return (
     <>
+    <div className="fixed top-0 right-0 mr-4 mt-4">
+      <SwatchBookIcon className="text-neutral-600 cursor-pointer" onClick={toggleTheme}/>
+    </div>
       <div className="flex h-full w-full select-none flex-col items-center justify-center gap-8 p-12 font-medium tabular-nums md:p-0">
         <div className="flex w-full items-center justify-center whitespace-nowrap leading-none">
           <div className="flex">
-            <div className="text-neutral-600 dark:text-zinc-600" style={{ fontSize: "12vw" }}>
+            <div className="text-neutral-600" style={{ fontSize: "12vw" }}>
               {timeStr}
             </div>
-            {/* <div className="flex flex-col gap-y-2 self-center text-orange-900 dark:text-orange-500">
-              <div>{amOrPm}</div>
-            </div> */}
           </div>
         </div>
         <div className="flex flex-wrap gap-2">
-          <Block title="Day name" data={dayStr}>
+          <Block theme={theme} title="Day name" data={dayStr}>
             <div className="flex w-fit gap-x-3">
               <Circles theme={theme} total={7} current={dayNum} />
             </div>
           </Block>
-          <Block title="Day" data={dayNumStr}>
+          <Block theme={theme} title="Day number" data={dayNumStr}>
             <div className="grid w-fit grid-cols-7 gap-3">
               <Circles theme={theme} total={daysInMonth} current={dayNum} />
             </div>
           </Block>
-          <Block title="Month" data={format(time, "MMMM")}>
+          <Block theme={theme} title="Month" data={format(time, "MMMM")}>
             <div className="grid w-fit grid-cols-3 gap-3">
               <Circles theme={theme} total={12} current={monthNum} />
             </div>
           </Block>
-          <Block title="Year" data={format(time, "yyyy")} />
-          <Block title="Week" data={format(time, "w")}>
+          <Block theme={theme} title="Year" data={format(time, "yyyy")} />
+          <Block theme={theme} title="Week" data={format(time, "w")}>
             <div className="grid w-fit grid-cols-7 gap-3">
               <Circles theme={theme} total={weeksInYear} current={weekNum} />
             </div>
           </Block>
-          <Block title="Quarter" data={quarterStr}>
+          <Block theme={theme} title="Quarter" data={quarterStr}>
             <div className="grid w-fit grid-cols-4 gap-3">
               <Circles theme={theme} total={4} current={quarterNum} />
             </div>
